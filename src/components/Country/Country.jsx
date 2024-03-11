988daf53cc08e6fe8cd7a891b91b2ec7f13df8a4
mdsobayelhossain <mdsobayelhossain@gmail.com>
2024-03-11 13:19:48 +0600

    added flags section

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,10 +1,13 @@
 import './Country.css'
 const Country = ({country}) => {
-    const {name, flags} = country;
+    const {name, flags, area, capital, population} = country;
     return (
         <div className="country">
             <h3>Name: {name?.common}</h3>
+            <h4>Capital: {capital}</h4>
             <img src={flags.png} alt="" />
+            <h4>Population: {population}</h4>
+            <h4>Area: {area}</h4>
         </div>
     );
 };
